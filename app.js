8e88ec99a43887b8f4816d5b30a5dcb7ff10bdfe
zimbatm <zimbatm@zimbatm.com>
2014-03-10 12:46:04 +0000

    self mugshot

--- app.js
+++ app.js
@@ -12,47 +12,115 @@ navigator.getMedia = (
 window.URL || (window.URL = window.webkitURL);
 window.audioContext || (window.audioContext = window.webkitAudioContext);
 
-var CommentBox = React.createClass({
+function once(fn) {
+	return function () {
+		if (fn.hasRun) return;
+		fn.hasRun = true;
+		fn.apply(this, arguments);
+	}
+}
+
+function getMugShot(success, error) {
+	navigator.getMedia(
+		{ video: true }, //, audio: true },
+		function(stream) {
+			var canvas = document.createElement("canvas");
+			var vid = document.createElement("video");
+			vid.autoplay = true;
+			vid.src = window.URL.createObjectURL(stream);
+
+			//document.body.appendChild(vid);
+			
+			vid.onplaying = once(function() {
+				vid.pause(); // No need
+				
+				canvas.width = vid.videoWidth;
+				canvas.height = vid.videoHeight;
+				canvas.getContext("2d").drawImage(vid, 0, 0);
+
+				success(canvas.toDataURL("image/png"));
+			});
+			vid.onerror = error;
+		},
+		error
+	);
+	
+}
+
+var Portrait = React.createClass({
+	handleClick: function() {
+		this.props.onClick(this.props.nick);
+	},
 	render: function() {
 		return (
-			<div className="commentBox">
-				Hello, world! I am a CommentBox.
+			<div className="portrait" onClick={this.handleClick}>
+				<div className="nick">{this.props.nick}</div>
+				<img src={this.props.image} height="160" />
 			</div>
 		);
 	}
 });
 
+var PersonList = React.createClass({
+	handleLiveToggle: function() {
+		console.log("Toggle");
+	},
+	render: function() {
+		var portraitNodes = this.props.persons.map(function (person) {
+			return <Portrait nick={person.nick} image={person.image} />
+		});
+		return (
+			<div className="personList">
+				<Portrait nick={this.props.self.nick} image={this.props.self.image} onClick={this.handleLiveToggle} />
+				{portraitNodes}
+			</div>
+		)
+	}
+});
+
+var PresenceBox = React.createClass({
+	getSelfMugshot: function() {
+		var self = this;
+		getMugShot(
+			function(newImage) {
+				var newSelf = {
+					nick: self.state.self.nick,
+					image: newImage
+				};
+
+				console.log("new self", newSelf);
+
+				self.setState({self: newSelf});	
+			},
+			function(error) {
+				console.log("mugshot error", error);
+			}
+		);
+	},
+	
+	getInitialState: function() {
+		var self = {
+			nick: "zimbatm",
+			image: "unknown.gif",
+		};
+		return {
+			self: self,
+			others: [],
+		};
+	},
+	componentWillMount: function() {
+		this.getSelfMugshot();
+	},
+	render: function() {
+		return (
+			<div className="presenceBox">
+				<PersonList self={this.state.self} persons={this.state.others} />
+			</div>
+		);
+	}
+})
+
 React.renderComponent(
-	<CommentBox />,
+	<PresenceBox />,
 	document.getElementById('content')
 );
-
-
-
-// var portrait = document.getElementById("portrait");
-// var portraitVid = document.getElementById("portraitVid");
-// var b_context = portrait.getContext("2d");
-// var snapshotBtn = document.getElementById("snapshot");
-
-// portraitVid.autoplay = true;
-// snapshotBtn.onclick = getPortrait;
-
-// navigator.getMedia(
-//	{ video: true },// audio: true },
-//	function(stream) {
-//		portraitVid.src = window.URL.createObjectURL(stream);
-//	},
-//	console.log
-// );
-
-// function getPortrait() {
-//	portrait.width = portraitVid.clientWidth;
-//	portrait.height = portraitVid.clientHeight;
-//	b_context.drawImage(
-//		portraitVid,
-//		0,
-//		0,
-//		portrait.width,
-//		portrait.height
-//	);
-// }
